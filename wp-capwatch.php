55e7b5f9fcf77d3e4d26d4654d9544cd79897d6f
Nick McLarty <nick@inick.net>
2014-08-24 16:19:41 -0500

    Initial commit

---
+++ wp-capwatch.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: wp-CAPWATCH for Unit Websites
+ * Plugin URI: http://www.inick.net
+ * Description: This plugin uses CAPWATCH data to populate fields on a unit website with membership data.
+ * Version: 0.1.0
+ * Author: Nick McLarty
+ * Author URI: https://www.inick.net
+ * License: GPLv3
+ */
+
