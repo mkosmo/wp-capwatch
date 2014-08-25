6859f880e25155dc2dd57f7e553ef0ecc23e3939
Nick McLarty <nick@inick.net>
2014-08-24 23:25:13 -0500

    Add script and style enqueueing

--- wp-capwatch.php
+++ wp-capwatch.php
@@ -19,6 +19,13 @@ include_once plugin_dir_path( __FILE__ ) . 'upload.php';
 register_activation_hook( __FILE__, 'capwatch_install' );
 register_deactivation_hook( __FILE__, 'capwatch_uninstall' );
 
+function wp_capwatch_enqueue_scripts() {
+	wp_enqueue_script( 'jquery-ui-sortable' );
+	wp_enqueue_style( 'wp-capwatch', plugin_dir_url( __FILE__ ) . 'wp-capwatch.css', NULL, '0.1.0' );
+}
+add_action( 'admin_init', 'wp_capwatch_enqueue_scripts' );
+
+
 function wp_capwatch_update_duty_position_order() {
 
 	global $wpdb;
