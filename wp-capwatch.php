91ddb9677a4bbadb90b5498b2252a8a236d15bec
Matthew J. Kosmoski <mkosmo@gmail.com>
2018-12-18 15:39:11 -0600

    Assistants and Emails (#5)
    * Update shortcodes.php to show assistants and use mailto links.  Closes #3 and #4 
    * Local version number 1.0.0-451a

--- wp-capwatch.php
+++ wp-capwatch.php
@@ -3,7 +3,7 @@
  * Plugin Name: wp-CAPWATCH for Unit Websites
  * Plugin URI: http://www.inick.net
  * Description: This plugin uses CAPWATCH data to populate fields on a unit website with membership data.
- * Version: 1.0.0
+ * Version: 1.0.0-451a
  * Author: Nick McLarty
  * Author URI: https://www.inick.net
  * License: GPLv3
